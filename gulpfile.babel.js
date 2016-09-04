d6a9a5c0a0b52de91dfcfb1ba828246ebc235e5f
Brian Montana <Brian.Montana@blastradius.com>
2016-09-04 12:50:10 -0500

    Removal of unused files and dependancies.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,41 +15,6 @@ import autoprefixer from 'autoprefixer';
 
 const $ = gulpLoadPlugins();
 
-// Optimize images, only looking at the main four. SVG, PNG, JPG and GIF. SVGs can be optimized further by the designer by reducing paths, etc
-gulp.task('images', () => {
-  return gulp.src('app/images/**/*.{svg,png,jpg,gif}')
-    .pipe($.cache($.imagemin()))
-    .pipe(gulp.dest('dist/images'))
-    .pipe($.size({title: 'images'}));
-});
-
-// Copy all files at the root level (app)
-gulp.task('copy', () => {
-  return gulp.src([
-    'app/*',
-    'app/*.html',
-    'node_modules/apache-server-configs/dist/.htaccess',
-    '!tmp-'
-  ], {
-    dot: true
-  })
-    .pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'copy'}));
-});
-
-//Run Sass Lint before compiling
-//Checks setup of Sass files and confirms they match the standards
-gulp.task('style-lint', () => {
-  return gulp.src('app/**/*.scss')
-    .pipe($.stylelint({
-      reporters: [
-        {formatter: 'string', console: true}
-      ],
-      debug: true,
-      syntax: 'scss'
-    }))
-});
-
 // Compile and automatically prefix stylesheets, postCSS can be reviewed for less than modern browser fall backs
 gulp.task('styles', () => {
   // For best performance, don't add Sass partials to `gulp.src` just globby glob it
@@ -65,29 +30,9 @@ gulp.task('styles', () => {
       outputStyle: 'expanded'
     }).on('error', $.sass.logError))
     .pipe($.postcss([autoprefixer()]))
-    .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest('dist'))
+    .pipe($.sourcemaps.write('./dist'))
+    .pipe(gulp.dest('dist/styles'))
     .pipe($.size({title: 'styles'}));
-});
-
-// Concatenate and minify JavaScript
-gulp.task('scripts', () => {
-  return gulp.src(['./app/scripts/main.js'])
-    .pipe($.concat('main.min.js'))
-    .pipe($.uglify({preserveComments: 'some'}))
-    // Output files
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe($.size({title: 'scripts'}));
-});
-
-// Scan your HTML for assets & optimize it
-gulp.task('html', () => {
-  return gulp.src('dist/**/*.html')
-    // Minify any HTML
-    .pipe($.if('*.html', $.htmlmin()))
-    // Output files
-    .pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'html'}));
 });
 
 // Clean output directory (dist/temp)
@@ -100,23 +45,18 @@ gulp.task('serve', () => {
   runSequence('default');
   browserSync.init({
     server: {
-      baseDir: './dist'
+      baseDir: './'
     }
   });
   //Each watch task is specific to how to control the direction of tasks
-  gulp.watch('app/scripts/*.js', ['scripts']);
   gulp.watch('app/styles/**/*.scss', ['styles']);
   //Watch index for any changes to landing page and run scripts
-  gulp.watch('app/index.html', ['default']);
-  gulp.watch('app/**/*.html', ['html']);
-  gulp.watch('app/images/**/*.{svg,png,jpg,gif}', ['images']);
+  gulp.watch('index.html', ['default']);
 });
 
 // Build production files, the default task
 gulp.task('default', ['clean'], () => {
   runSequence(
-    'styles',
-    'copy',
-    ['scripts', 'html', 'images']
+    'styles'
   );
 });
